603a93025d5a5f94adfe2e1b961650afd273b175
Green Wang <cocowool@qq.com>
2019-08-23 20:44:24 +0800

    init project

---
+++ dataset-gernerator.py
@@ -0,0 +1,10 @@
+# Dataset 测试数据生成脚本
+# 二维数据格式
+# Col1 Col2 Col3 Col4 Col5
+# Wang 32   12   13    Male
+# Zhang 32   11  24    Female
+# https://mockaroo.com
+
+import numpy as numpy
+from numpy import random
+
