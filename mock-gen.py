5bb74b7048af250a5901334ae467ea56f93fbe8d
Green Wang <cocowool@qq.com>
2019-08-27 17:18:18 +0800

    finish gen list function

--- mock-gen.py
+++ mock-gen.py
@@ -9,9 +9,45 @@
 # python3 dataset-generator.py resultType length range 
 # Goal 1: 支持生成一维数字型数组
 
-import numpy as numpy
+import os,sys,getopt
+import numpy as np
 from numpy import random
 
-def if __name__ == "__main__":
-    pass
+# 入口函数，第一个版本支持生成一维数字型数组
+def main(argv):
+    resultType = 'l'
+    length = 100
+    range = 10000
 
+    print("Welcome usage mock-gen!")
+
+    try:
+        opts, args = getopt.getopt(argv, "hr:l:n:", ["resultType=","length=","range="])
+        # print(opts)
+    except getopt.GetoptError:
+        print("Usage: python3 mock-gen.py -resultType <typename> -length <length> -range <range>")
+
+    for opt, arg in opts:
+        if opt in ("-r", "--resultType"):
+            resultType = arg
+        elif opt in ("-l", "--length"):
+            length = int(arg)
+        elif opt in ("-n", "--range"):
+            range = int(arg)
+
+    print( get_random_list(length, range) )
+
+def get_random_list(length, max):
+    i = 0
+    result = []
+
+    while i < length :
+        result.append(np.random.randint(0,max))
+        i = i + 1
+
+    return result
+
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
